fcf5dfc4ee8a5ca38ea2a534dee4089b68686eee
yby1997 <frmpjmattlemage@gmail.com>
2022-04-20 15:00:38 -0700

    clean new branch

--- src/main/java/com/example/HackFSE/repository/CompanyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.HackFSE.repository;
-
-import com.example.HackFSE.model.Company;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CompanyRepository extends MongoRepository<Company, String> {
-}
